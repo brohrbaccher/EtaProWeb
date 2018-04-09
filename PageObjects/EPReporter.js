36fb524e4649031e2869580b2697fd032909f012
Andrew Call <w.andrew.call@gmail.com>
2018-04-09 12:02:38 -0600

    EPReporter v1

--- PageObjects/EPReporter.js
+++ PageObjects/EPReporter.js
@@ -1,6 +1,16 @@
+var login = {
+    login() {
+        this.navigate()
+        this.api.acceptAlert()
+        this.waitForElementVisible('@verifiableText', 5000)
+        return this.expect.element('@verifiableText').to.be.visible
+    }
+}
+
 module.exports = {
-    url: '',
-    commands: [],
+    url: 'http://epuser:Amherst716!@18.219.202.133/etaproweb/',
+    commands: [login],
     elements: {
+        verifiableText: '#ctl00_splitter_ctl17',
     }
 }
--- Tests/EPReporterTests.js
+++ Tests/EPReporterTests.js
@@ -1,5 +1,10 @@
 const data = require('../Supporting/data')
 
 module.exports = {
+    'login' : browser => {
+        main = browser.page.EPReporter()
+        main.login()
+        browser.end()
+    }
 
 }
