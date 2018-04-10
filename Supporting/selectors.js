dc225eaef9801f6af6b101dc2ef79fb20c5feccd
JypsyJill <jillyn_oc_ca@yahoo.com>
2018-04-10 14:04:16 -0600

    Jills-Automation

---
+++ Supporting/selectors.js
@@ -0,0 +1,19 @@
+module.exports = {
+    //  XPath below this point
+    locationExpanderArrow: '(//span[@class="dxtv-elbNoLn"])[30]',
+    boilerExpanderArrow: '(//span[@class="dxtv-elbNoLn"])[36]',
+    concernsViewerTab: '//div[@id="ctl00_splitter_formLayoutHeader_butConcernsViewer"]',
+    assetFilterButton: '//*[@id="splitter_ASPxPanel1_grid_DXFREditorcol1_I"]',
+    checkBoxChecked: '//*[@id="splitter_ASPxPanel1_grid_DXFilterBar_CBFilterEnabled_S_D"]',
+    
+    
+     // CSS below this point
+
+   
+    serverLogin: 'a[id="ctl00_splitter_formLayoutHeader_hlServerLogin"]',
+    demoServer: 'input[class="dxeEditArea_Metropolis dxeEditAreaSys"]',
+    createFilter: 'a[class="dxgvFilterBarLink_Metropolis"]',
+    concernsViewerUnitFilter: 'input[id="splitter_ASPxPanel1_grid_DXFREditorcol1_I"]',
+   
+
+}
--- Tests/ConcernsViewerTests.js
+++ Tests/ConcernsViewerTests.js
@@ -1,5 +1,45 @@
 const data = require('../Supporting/data')
+const selector = require('../Supporting/selectors')
 
+// Jill's work
 module.exports = {
+before: browser =>{
+browser.url('http://epuser:Amherst716!@18.219.202.133/etaproweb/')
+.acceptAlert()
 
+},
+// after: browser => {
+//     browser.end()
+
+// },
+
+'Navigate EtaProWeb': browser=> {
+browser
+.waitForElementVisible(selector.serverLogin, 3000)
+.setValue(selector.demoServer, "Demonstration")
+.click(selector.serverLogin)
+.pause(2000)
+.useXpath()
+.waitForElementVisible(selector.locationExpanderArrow, 15000)
+.click(selector.locationExpanderArrow)
+.pause(20000)
+.expect.element(selector.boilerExpanderArrow).to.be.visible
+browser
+.waitForElementVisible(selector.boilerExpanderArrow, 15000)
+.pause(15000)
+.waitForElementVisible(selector.concernsViewerTab, 5000)
+.click(selector.concernsViewerTab)
+.pause(25000)
+.frame(0)
+.waitForElementVisible(selector.assetFilterButton, 15000)
+.useCss()
+.setValue(selector.concernsViewerUnitFilter, "Heater")
+.waitForElementVisible(selector.concernsViewerUnitFilter, 10000)
+.click(selector.createFilter)
+.pause(15000)
+.useXpath()
+.expect.element(selector.checkBoxChecked).is.visible
+
+
+},
 }
