402a00edc7d5cd402fecf9f39dcccc01f3a35898
Andrew Call <w.andrew.call@gmail.com>
2018-04-12 12:05:22 -0600

    cleanup and fixes

--- Supporting/selectors.js
+++ Supporting/selectors.js
@@ -5,15 +5,13 @@ module.exports = {
     concernsViewerTab: '//div[@id="ctl00_splitter_formLayoutHeader_butConcernsViewer"]',
     assetFilterButton: '//*[@id="splitter_ASPxPanel1_grid_DXFREditorcol1_I"]',
     checkBoxChecked: '//*[@id="splitter_ASPxPanel1_grid_DXFilterBar_CBFilterEnabled_S_D"]',
-    
-    
-     // CSS below this point
 
-   
+
+    // CSS below this point
     serverLogin: 'a[id="ctl00_splitter_formLayoutHeader_hlServerLogin"]',
     demoServer: 'input[class="dxeEditArea_Metropolis dxeEditAreaSys"]',
     createFilter: 'a[class="dxgvFilterBarLink_Metropolis"]',
     concernsViewerUnitFilter: 'input[id="splitter_ASPxPanel1_grid_DXFREditorcol1_I"]',
-   
+
 
 }
--- Tests/ConcernsViewerTests.js
+++ Tests/ConcernsViewerTests.js
@@ -3,43 +3,34 @@ const selector = require('../Supporting/selectors')
 
 // Jill's work
 module.exports = {
-before: browser =>{
-browser.url('http://epuser:Amherst716!@18.219.202.133/etaproweb/')
-.acceptAlert()
+    before: browser => {
+        browser.url('http://epuser:Amherst716!@18.219.202.133/etaproweb/')
+            .acceptAlert()
 
-},
-// after: browser => {
-//     browser.end()
+    },
 
-// },
-
-'Navigate EtaProWeb': browser=> {
-browser
-.waitForElementVisible(selector.serverLogin, 3000)
-.setValue(selector.demoServer, "Demonstration")
-.click(selector.serverLogin)
-.pause(2000)
-.useXpath()
-.waitForElementVisible(selector.locationExpanderArrow, 15000)
-.click(selector.locationExpanderArrow)
-.pause(20000)
-.expect.element(selector.boilerExpanderArrow).to.be.visible
-browser
-.waitForElementVisible(selector.boilerExpanderArrow, 15000)
-.pause(15000)
-.waitForElementVisible(selector.concernsViewerTab, 5000)
-.click(selector.concernsViewerTab)
-.pause(25000)
-.frame(0)
-.waitForElementVisible(selector.assetFilterButton, 15000)
-.useCss()
-.setValue(selector.concernsViewerUnitFilter, "Heater")
-.waitForElementVisible(selector.concernsViewerUnitFilter, 10000)
-.click(selector.createFilter)
-.pause(15000)
-.useXpath()
-.expect.element(selector.checkBoxChecked).is.visible
-
-
-},
+    'Navigate EtaProWeb': browser => {
+        browser
+            .waitForElementVisible(selector.serverLogin, 3000)
+            .setValue(selector.demoServer, "Demonstration")
+            .click(selector.serverLogin)
+            .useXpath()
+            .pause(2000)
+            .waitForElementVisible(selector.locationExpanderArrow, 15000)
+            .click(selector.locationExpanderArrow)
+            .waitForElementVisible(selector.boilerExpanderArrow, 20000)
+            .expect.element(selector.boilerExpanderArrow).to.be.visible
+        browser
+            .waitForElementVisible(selector.boilerExpanderArrow, 20000)
+            .waitForElementVisible(selector.concernsViewerTab, 20000)
+            .click(selector.concernsViewerTab)
+            .frame(0)
+            .waitForElementVisible(selector.assetFilterButton, 20000)
+            .useCss()
+            .waitForElementVisible(selector.concernsViewerUnitFilter, 20000)
+            .setValue(selector.concernsViewerUnitFilter, "Heater")
+            .useXpath()
+            .expect.element(selector.checkBoxChecked).is.visible
+        browser.end()
+    },
 }
